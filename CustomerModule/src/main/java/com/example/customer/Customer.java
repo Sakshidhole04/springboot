efcc68d4ec16af9cd9430cfd5f980eb8bc8e2187
Xtrem <Xtrem@DESKTOP-JH9CR42>
2023-01-20 22:42:18 +0530

    three commit

---
+++ CustomerModule/src/main/java/com/example/customer/Customer.java
@@ -0,0 +1,66 @@
+package com.example.customer;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer 
+{
+	private Integer id;
+	private String name;
+	private String phone;
+	private String email;
+	
+	public Customer() {
+		super();
+	}
+
+	public Customer(Integer id, String name, String phone, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.email = email;
+	}
+	@Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", phone=" + phone + ", email=" + email + "]";
+	}
+
+}
